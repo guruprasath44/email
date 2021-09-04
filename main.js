72d53c31c2396cdfdc1143b41f586b2b20b9e28f
Guruprasath4401 <guruprasathindirakumar@gmail.com>
2021-09-04 21:16:06 +0530

    first commit

--- main.js
+++ main.js
@@ -1,46 +1,22 @@
-window.addEventListener("DOMContentLoaded", function () {
-
-    // get the form elements defined in your form HTML above
-  
-    var form = document.getElementById("test-form");
-    var button = document.getElementById("test-form-submit");
-    var status = document.getElementById("status");
-  
-    // Success and Error functions for after the form is submitted
-  
-    function success() {
-      form.reset();
-      status.classList.add('success');
-      status.innerHTML = "Thanks!";
+var form = document.getElementById("test-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status-form");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
     }
-  
-    function error() {
-      status.classList.add('error');
-      status.innerHTML = "Oops! There was a problem.";
-    }
-  
-    // handle the form submission event
-  
-    form.addEventListener("submit", function (ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
+  }).then(response => {
+    status.classList.add('success');
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.classList.add('error');
+    status.innerHTML = "Oops! There was a problem submitting your form"
   });
-  
-  // helper function for sending an AJAX request
-  
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
+}
+form.addEventListener("submit", handleSubmit)
